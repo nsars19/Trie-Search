2ccc4e3c7346eceb0ded3c8e7fef6ecf9ca0129c
Nick S <nsardonini@gmail.com>
2021-05-17 13:48:09 -0400

    add test directory

---
+++ test/trie.test.js
@@ -0,0 +1,16 @@
+const { expect, test, describe } = require("@jest/globals");
+const Trie = require("../src/trie");
+
+describe("Trie", () => {
+  test("returns an object", () => {
+    const trie = new Trie();
+    expect(typeof trie).toBe("object");
+  });
+
+  test("adds properly", () => {
+    const trie = new Trie();
+    trie.add("Hi");
+    const childVal = trie.children["h"].children["i"].val;
+    expect(childVal).toBe("Hi");
+  });
+});
